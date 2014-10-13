4f746f1f8d9bfca0aa8d68a6255b85da3f8424ad
Jing Jin <jing@konsu.lt>
2014-10-13 15:48:38 -0700

    Added basic profile and edit profile UI with minimal styling.

--- app/src/main/java/org/chicktech/fragments/ProfileFragment.java
+++ app/src/main/java/org/chicktech/fragments/ProfileFragment.java
@@ -1,7 +1,65 @@
 package org.chicktech.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class ProfileFragment extends Fragment {
+import org.chicktech.R;
 
+
+public class ProfileFragment extends Fragment implements EditProfileFragment.OnSaveListener {
+    Button btnEdit;
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        //TODO: Add args here
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //TODO: Retrieve args here
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        btnEdit = (Button) v.findViewById(R.id.btnEdit);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editProfile();
+            }
+        });
+
+        return v;
+    }
+
+    public void editProfile() {
+        EditProfileFragment editProfileFragment = EditProfileFragment.newInstance();
+        editProfileFragment.setListener(this);
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.flContent, editProfileFragment);
+        ft.addToBackStack("Edit Profile");
+        ft.commit();
+    }
+
+    @Override
+    public void onProfileSave() {
+        //TODO: Update Profile
+    }
 }
