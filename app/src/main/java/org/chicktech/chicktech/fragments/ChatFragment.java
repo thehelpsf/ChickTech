3d73063782c62f0f92158253b3e761eb9e1b6203
Kenan Pulak <kepulak@gmail.com>
2014-10-15 23:13:36 -0700

    started chat ui

--- app/src/main/java/org/chicktech/chicktech/fragments/ChatFragment.java
+++ app/src/main/java/org/chicktech/chicktech/fragments/ChatFragment.java
@@ -1,8 +1,57 @@
 package org.chicktech.chicktech.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.chicktech.chicktech.R;
+import org.chicktech.chicktech.adapters.EventArrayAdapter;
+import org.chicktech.chicktech.models.ChatMessage;
+
+import java.util.ArrayList;
 
 public class ChatFragment extends Fragment {
 
+    Button btnSend;
+    EditText etMessage;
+    ListView lvMessages;
+    EventArrayAdapter aMessages;
+    ArrayList<ChatMessage> messages;
+
+    public static ChatFragment newInstance() {
+        ChatFragment fragment = new ChatFragment();
+        Bundle args = new Bundle();
+        //TODO: Add args here
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //TODO: Retrieve args here
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_chat, container, false);
+        btnSend = (Button) v.findViewById(R.id.btnSend);
+        etMessage = (EditText) v.findViewById(R.id.etMessage);
+        lvMessages = (ListView) v.findViewById(R.id.lvMessages);
+
+        return v;
+    }
 
 }
