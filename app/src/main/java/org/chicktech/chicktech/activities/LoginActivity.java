6b03163c52aece87d9cfffe88f7f0af678274d35
Jing Jin <jing@konsu.lt>
2014-11-07 22:49:41 -0800

    Fix # 127 Close keyboard before opening drawer.

--- app/src/main/java/org/chicktech/chicktech/activities/LoginActivity.java
+++ app/src/main/java/org/chicktech/chicktech/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package org.chicktech.chicktech.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
